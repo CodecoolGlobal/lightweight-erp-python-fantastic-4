c8856036143ab3b5a73574fb54b3c0e7b08d325e
Olivér Gál <galoliver05@gmail.com>
2019-08-21 16:54:28 +0200

    Add 'add' , 'remove' and 'update' to hr.py

--- hr/hr.py
+++ hr/hr.py
@@ -38,9 +38,15 @@ def start_module():
             if option == "1":
                 filecontent = add(filecontent)
             elif option == "2":
-                pass
+                remove_id = ui.get_inputs(
+                    ['ID: '], 'Enter ID to remove item from the list'
+                )[0]
+                remove(filecontent, remove_id)
             elif option == "3":
-                pass
+                update_id = ui.get_inputs(
+                    ['ID: '], 'Enter ID to update item in the list'
+                )[0]
+                update(filecontent, update_id)
             elif option == "4":
                 ui.print_result(get_oldest_person(
                     filecontent), "Oldest person or people: ")
@@ -87,8 +93,13 @@ def add(table):
         list: Table with a new record
     """
 
-    # your code
-
+    title_list = ['ID', 'Name', 'Year']
+    title_list.pop(0)
+    for item_index, item in enumerate(title_list):
+        title_list[item_index] += ': '
+    new_line = ui.get_inputs(title_list, 'Enter the following items')
+    new_line.insert(0, common.generate_random(table))
+    table.append(new_line)
     return table
 
 
@@ -104,7 +115,16 @@ def remove(table, id_):
         list: Table without specified record.
     """
 
-    # your code
+    id_found = False
+
+    for line_index, line in enumerate(table):
+        if line[0] == id_:
+            table.pop(line_index)
+            id_found = True
+            break
+
+    if id_found is False:
+        raise KeyError('ID not found')
 
     return table
 
@@ -121,7 +141,22 @@ def update(table, id_):
         list: table with updated record
     """
 
-    # your code
+    id_found = False
+    title_list = ['ID', 'Name', 'Year']
+    title_list.pop(0)
+    for item_index, item in enumerate(title_list):
+        title_list[item_index] += ': '
+    new_line = ui.get_inputs(title_list, 'Enter the following items')
+
+    for line_index, line in enumerate(table):
+        if line[0] == id_:
+            new_line.insert(0, line[0])
+            table[line_index] = new_line
+            id_found = True
+            break
+
+    if id_found is False:
+        raise KeyError('ID not found')
 
     return table
 
